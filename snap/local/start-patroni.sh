a76e6368cd41b71850d27e22fb2798197736dda7
Lukasz Wasylyk <45600996+lukasz-wasylyk@users.noreply.github.com>
2025-02-05 23:09:12 +0100

    Update start-patroni.sh test

--- snap/local/start-patroni.sh
+++ snap/local/start-patroni.sh
@@ -6,5 +6,11 @@ export LOCPATH="${SNAP}"/usr/lib/locale
 $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon --regid snap_daemon -- mkdir -p $SNAP_DATA/etc/patronii
 ls -la $SNAP_DATA/etc
 
+if [ ! -e $SNAP_DATA/etc/patronii/patroni.yaml ]; then
+  cp $SNAP/config/patroni.yaml $SNAP_DATA/etc/patronii
+fi
+ls -la $SNAP_DATA/etc/patronii
+cat $SNAP_DATA/etc/patronii/patroni.yaml
+
 $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
   --regid snap_daemon -- $SNAP/usr/bin/patroni $SNAP_DATA/etc/patroni/patroni.yaml "$@"
