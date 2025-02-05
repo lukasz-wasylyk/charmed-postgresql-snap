f0c504699ab049ac8cf47cb894bf25cd04cff46f
Lukasz Wasylyk <45600996+lukasz-wasylyk@users.noreply.github.com>
2025-02-05 23:21:11 +0100

    Update start-patroni.sh

--- snap/local/start-patroni.sh
+++ snap/local/start-patroni.sh
@@ -3,14 +3,14 @@
 # For security measures, daemons should not be run as sudo. Execute patroni as the non-sudo user: snap_daemon.
 export LOCPATH="${SNAP}"/usr/lib/locale
 
-$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon --regid snap_daemon -- mkdir -p $SNAP_DATA/etc/patronii
+$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon --regid snap_daemon -- mkdir -p $SNAP_DATA/etc/patroni
 ls -la $SNAP_DATA/etc
 
-if [ ! -e $SNAP_DATA/etc/patronii/patroni.yaml ]; then
-  cp $SNAP/config/patroni.yaml $SNAP_DATA/etc/patronii
+if [ ! -e $SNAP_DATA/etc/patroni/patroni.yaml ]; then
+  cp $SNAP/config/patroni.yaml $SNAP_DATA/etc/patroni
 fi
-ls -la $SNAP_DATA/etc/patronii
-cat $SNAP_DATA/etc/patronii/patroni.yaml
+ls -la $SNAP_DATA/etc/patroni
+cat $SNAP_DATA/etc/patroni/patroni.yaml
 
 $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
   --regid snap_daemon -- $SNAP/usr/bin/patroni $SNAP_DATA/etc/patroni/patroni.yaml "$@"
