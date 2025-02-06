57ddc26715023fc2d21c5dfbebaec54f3430ac30
Lukasz Wasylyk <45600996+lukasz-wasylyk@users.noreply.github.com>
2025-02-06 17:40:07 +0100

    Update start-patroni.sh - raft to be created by snap daemon

--- snap/local/start-patroni.sh
+++ snap/local/start-patroni.sh
@@ -3,7 +3,7 @@
 # For security measures, daemons should not be run as sudo. Execute patroni as the non-sudo user: snap_daemon.
 export LOCPATH="${SNAP}"/usr/lib/locale
 
-for dir in "$SNAP_DATA/etc/patroni" "$SNAP_COMMON/postgresql" "$SNAP_DATA/postgresql"; do
+for dir in "$SNAP_DATA/etc/patroni" "$SNAP_COMMON/postgresql" "$SNAP_DATA/postgresql" "$SNAP_COMMON/raft"; do
     $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon --regid snap_daemon -- mkdir -p "$dir"
 done
 
