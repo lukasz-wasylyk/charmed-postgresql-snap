9453a0d1d55c4361363cda380c41b7178eb07461
Lukasz Wasylyk <45600996+lukasz-wasylyk@users.noreply.github.com>
2025-02-11 22:17:57 +0100

    Create start_postgresql_standalone.sh

---
+++ snap/local/start_postgresql_standalone.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# For security measures, applications should not be run as sudo.
+export LOCPATH="${SNAP}"/usr/lib/locale
+export PGDATA=$SNAP_DATA/pgsql/data
+
+export USER_ID="584788" # hack - we need to make sure we can run setpriv. Maybe we can create some user instead in context of snap only ?
+"${SNAP}/usr/bin/setpriv" --clear-groups --reuid $USER_ID --regid $USER_ID -- "${SNAP}/usr/lib/postgresql/14/bin/postgres" -k /tmp -D "${PGDATA}"
