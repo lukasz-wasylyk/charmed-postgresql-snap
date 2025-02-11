68ae4810affcff6bf064df108091b15ad52ee202
Lukasz Wasylyk <45600996+lukasz-wasylyk@users.noreply.github.com>
2025-02-11 15:12:15 +0100

    Update start-patroni.sh - rework to use vars

--- snap/local/start-patroni.sh
+++ snap/local/start-patroni.sh
@@ -2,15 +2,15 @@
 
 # For security measures, daemons should not be run as sudo. Execute patroni as the non-sudo user: snap_daemon.
 export LOCPATH="${SNAP}"/usr/lib/locale
-
-$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon --regid snap_daemon -- mkdir -p $SNAP_DATA/etc/patroni
-$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon --regid snap_daemon -- mkdir -p $SNAP_COMMON/postgresql
-$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon --regid snap_daemon -- mkdir -p $SNAP_DATA/postgresql
-$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon --regid snap_daemon -- mkdir -p $SNAP_COMMON/raft
+export USER_ID="snap_daemon"
+$SNAP/usr/bin/setpriv --clear-groups --reuid $USER_ID --regid $USER_ID -- mkdir -p $SNAP_DATA/etc/patroni
+$SNAP/usr/bin/setpriv --clear-groups --reuid $USER_ID --regid $USER_ID -- mkdir -p $SNAP_COMMON/postgresql
+$SNAP/usr/bin/setpriv --clear-groups --reuid $USER_ID --regid $USER_ID -- mkdir -p $SNAP_DATA/postgresql
+$SNAP/usr/bin/setpriv --clear-groups --reuid $USER_ID --regid $USER_ID -- mkdir -p $SNAP_COMMON/raft
 
 if [ ! -e $SNAP_DATA/etc/patroni/patroni.yaml ]; then
-  $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon --regid snap_daemon -- cp $SNAP/config/patroni.yaml $SNAP_DATA/etc/patroni
+  $SNAP/usr/bin/setpriv --clear-groups --reuid $USER_ID --regid $USER_ID -- cp $SNAP/config/patroni.yaml $SNAP_DATA/etc/patroni
 fi
 
-$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
-  --regid snap_daemon -- $SNAP/usr/bin/patroni $SNAP_DATA/etc/patroni/patroni.yaml "$@"
+$SNAP/usr/bin/setpriv --clear-groups --reuid $USER_ID \
+  --regid $USER_ID -- $SNAP/usr/bin/patroni $SNAP_DATA/etc/patroni/patroni.yaml "$@"
